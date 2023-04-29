25a79b85154add7a7f47f9220c13222c5a26de8e
Life75 <austynwashington@yahoo.com>
2023-04-29 13:07:23 -0400

    need to work on image sizing for mobile

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="text-center text-xl mt-4 font-serif text-white">
-      <p class=".someSelector">Hello</p>
-      <p>
-        My name is Austyn Washington and this project is to showcase and teach Data
-        Structures and Algorithms through a visual representation
-      </p>
-    </div>
-  </template>
+   <div class="block text-center">
+    <span class="w-2/3">
+    <el-carousel class="items-center"  autoplay="true" height="900px">
+      <el-carousel-item v-for="item in 4" :key="item">
+      <div class="background">
+      </div>
+    
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </span>
+  </div>
+</template>
   
   <script setup lang="ts"></script>
   
@@ -14,5 +19,15 @@
     .someSelector {
       font-family: 'Muli';
     }
+
+    .background {
+      background: #eee url(../assets/church_img/Front-view.jpg) ;
+      background-position: 30% 30%;
+            width: 100%;
+            height:100%;
+            text-indent: -9999px;
+    }
+
+
 
   </style>
--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -6,20 +6,32 @@
     :ellipsis="true"
     background-color="#FBD46D"
     text-color="#07031A"
-    active-text-color="#85ce61"
+    active-text-color="07031A"
+    class="text-font"
   >
     <el-menu-item index="0" style="">
+      <img class="" src="https://img.icons8.com/glyph-neue/64/null/cross.png"/>
+
       <div class="flex flex-col text-center ">
-        <span class="test text-2xl" @click="homeRoute()">Jesus Can Help Ministries</span>
-        <span class="test text-xl text-gray-500">Pensacola</span>
+        <span class="text-font text-2xl" @click="homeRoute()">Jesus Can Help Ministries</span>
+        <span class="text-font text-xl text-gray-500 tracking-widest">Pensacola</span>
       </div>
+
       <router-link to="/"></router-link
     ></el-menu-item>
+    <span class="px-44"></span>
+    <el-menu-item style="" index="1">
+      <template #title> NEW HERE? </template>
+    </el-menu-item>
 
-    <el-sub-menu style="padding-left: 16em" index="1">
-      <template #title> New Here? </template>
-    </el-sub-menu>
+    <el-menu-item style="" index="1">
+      <template #title> EVENTS </template>
+    </el-menu-item>
    
+   
+    <el-menu-item style="" index="1">
+      <template #title> ABOUT US </template>
+    </el-menu-item>
   </el-menu>
 </template>
 
@@ -27,34 +39,19 @@
 import { ref } from "vue";
 import router from "../router/main";
 
-const props = defineProps({
-  title: { type: String, default: "" },
-});
+
 
 const activeIndex = ref("1");
-
-function sortingRoutChange(route: string) {
-  router.push({ path: `/Sorting/${route}` });
-}
-
-function dataStructureRouteChange(route: string) {
-  router.push({ path: `/DataStructures/${route}` });
-}
 
 function homeRoute() {
   router.push({ path: "/" });
 }
 
-function getTitle(): string {
-  if (props.title != "") {
-    return props.title;
-  }
-  return "Sorting Algorithms";
-}
+
 </script>
 
 <style>
-.test {
+.text-font {
   font-family: "Lora";
 }
 </style>
