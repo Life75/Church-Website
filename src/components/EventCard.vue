c7299afa7004ed35d2561673cfa5a27fe4c95061
Life75 <austynwashington@yahoo.com>
2024-06-03 23:46:04 -0400

    working on cosmetics

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class=" w-1/2 bg-white shadow-md hover:shadow-xl">
+    <div class=" w-[425px] bg-white shadow-md hover:shadow-xl">
         <div class="relative">
             <div class="absolute top-0  w-20 h-20 bg-purple-400 flex flex-col items-center justify-center text-lg font-semibold">
                 <p>22</p>
                 <p>MAR</p>
             </div>
-            <img class="h-full" src="https://images.pexels.com/photos/941555/pexels-photo-941555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-            <div class="absolute flex justify-center items-center pr-20 bottom-0 h-16 text-center w-full bg-gray-500 opacity-60 text-white text-2xl font-semibold">Student Gallery Unveiled</div>
+            <img class="h-full" src="https://images.pexels.com/photos/20479821/pexels-photo-20479821/free-photo-of-leaves-in-dew.jpeg" alt="">
+            <div class="absolute flex justify-center items-center pr-20 bottom-0 h-16 text-center w-full bg-gray-400  bg-opacity-50 text-white text-2xl font-semibold">
+                 <p class="">Student Gallery Unveiled</p></div>
         </div>
-        <div class="p-6">Event Card designed by Cody Sielawa for Mighty in the Midwest. Connect with them on Dribbble; the global community for designers and creative professionals.</div>
+        <div class="p-6 flex flex-col gap-5">
+
+            <p>Event Card designed by Cody Sielawa for Mighty in the Midwest. Connect with them on Dribbble; the global community for designers and creative professionals.
+            </p>
+        
+            <button class="btn bg-blue-300 rounded-sm w-32 ml-auto">More Details</button>
+        </div>
         
    </div>
 </template>
